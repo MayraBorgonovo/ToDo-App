969f627d79d148681f2f60465a35aa10d4a4ef47
Mayra Borgonovo <mayra.borgonovo@gmail.com>
2022-01-01 20:52:50 +1100

    Added Context

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -2,20 +2,22 @@
 // by setting React.FC as the type to the Todos variable, we can access all built-in props
 //in the angle brackets define our own props and the their type
 
-import Todo from "../models/todo";
+import { useContext } from "react";
+import { TodosContext } from "../store/todos-context";
 import TodoItem from "./TodoItem";
 import classes from "./Todos.module.css";
 
-const Todos: React.FC<{ items: Todo[]; onRemoveTodo: (id: string) => void }> = (
-  props
+const Todos: React.FC = (
 ) => {
+  const todosCtx = useContext(TodosContext);
+
   return (
     <ul className={classes.todos}>
-      {props.items.map((item) => (
+      {todosCtx.items.map((item) => (
         <TodoItem
           key={item.id}
           text={item.text}
-          onRemoveTodo={props.onRemoveTodo.bind(null, item.id)}
+          onRemoveTodo={todosCtx.removeTodo.bind(null, item.id)}
         />
       ))}
     </ul>
